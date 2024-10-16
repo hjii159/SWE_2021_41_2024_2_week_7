30e4b1ed765e1aca32daebdd14846b58f1451431
hjii <hjii158@naver.com>
2024-10-16 19:27:16 +0900

    main.py - initial version

---
+++ main.py
@@ -0,0 +1,26 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    # Your Codes
+    ## Do not use input() or print() in your function
+    ## Inputs (nums, target) will given as arguments and the output as
+    ## return value
+    flag = False
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if nums[i] == target-nums[j]:
+                ret = [i, j]
+                #print(f"i: {nums[i]}[{i}], j: {nums[j]}[{j}]")
+                break
+        if ret:
+            break
+    return ret
+
+"""
+def main():
+    nums = [3, 3]
+    target = 6
+    print(twoSum(nums, target))
+
+main()
+"""
